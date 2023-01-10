5c72b8ca60cd276f828e37b01b8ec7bd51b03378
rouclec <senatorasonganyi97@gmail.com>
2023-01-10 15:34:36 +0100

    added CORS

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "10kb" }));
 
 const authRouter = require("./routes/authRoutes");
