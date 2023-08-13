6c62d57e9da284f74c2ca149664ab3703ccdcb4e
Rouclec <senatorasonganyi97@gmail.com>
2023-08-13 12:10:06 +0100

    get user by token

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,4 +22,43 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 });
 
 exports.getAllUsers = getAll(User);
-exports.getUser = getOne(User)
+exports.getUser = catchAsync(async (req, res, next) => {
+  let token;
+  if (req.params.token) {
+    token = req.params.token
+  } else {
+    return res.status(401).json({
+      status: "Server error",
+      message: "Please provide a token",
+    });
+  }
+
+  // 2) verify token
+  const verifiedToken = await promisify(jwt.verify)(
+    token,
+    process.env.JWT_SECRET
+  );
+
+  //3) check if token has expired
+  if (verifiedToken.exp * 1000 < Date.now()) {
+    return res.status(401).json({
+      status: "Unauthorized",
+      message: "Invalid Token",
+    });
+  }
+  //4) check if user exists
+  const user = await User.findById(verifiedToken.id);
+  if (!user) {
+    return res.status(401).json({
+      status: "Unauthorized",
+      message: "Something went wrong",
+    });
+  }
+
+  return next(
+    res.status(200).json({
+      status: 'OK',
+      data: user
+    })
+  )
+})
