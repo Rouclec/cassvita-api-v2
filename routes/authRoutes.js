b00739b43bd5c7d31a17b2022a93ef75390fbc79
Rouclec <senatorasonganyi97@gmail.com>
2023-08-23 12:23:36 +0100

    refresh token fix

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,16 +7,18 @@ const {
   forgotPassword,
   resetPassword,
   updatePasswword,
+  refreshToken,
 } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/login", login, protect);
+router.post('/refresh-token', refreshToken)
 router.post("/forgot-password", forgotPassword);
 router.patch("/reset-password/:token", resetPassword);
 
 router.use(protect); //every route below this line, will pass through the protect middleware first
 router.post("/add-user", restrictTo("accountant", "admin", "procurement-officer"), addUser);
-router.patch('/update-password',updatePasswword)
+router.patch('/update-password', updatePasswword)
 
 module.exports = router;
